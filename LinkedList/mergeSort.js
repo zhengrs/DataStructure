346f8c227502db81eda3af4993caacd438e93cb5
Zhizunbao <zhengrs@g.clemson.edu>
2016-04-12 18:36:56 -0400

    Update mergeSort.js

--- LinkedList/mergeSort.js
+++ LinkedList/mergeSort.js
@@ -1,45 +1,17 @@
 (function(){
 
-	var LinkedList = function(){
-		this.head = null;
-	}, LinkedNode = function(data){
-		this.data = data;
-		this.next = null;
-	};
+	var merge = function( h1, h2 ){
+		if( !h1 )	return h2;
+		if( !h2 )	return h1;
 
-	LinkedList.prototype.append = function(data){
-		var newNode = new LinkedNode(data), node;
-		if(!this.head){
-			this.head = newNode;
-			return;
-		}
+		var head = null,
+				node1 = h1,
+				node2 = h2;
+				node = head;
 
-		node = this.head;
-		while(node.next){
-			node = node.next;
-		}
-		node.next = newNode;
-	};
-
-	var print = function(head){
-		while(head){
-			console.log(head.data);
-			head = head.next;
-		}
-	};
-
-	var mergeSortedLinkedList = function(head1, head2){
-
-		if( !head1 ) return head2; 
-		if( !head2 ) return head1;
-
-		var node1 = head1, 
-				node2 = head2,
-				head, node;  // node always points to the last node of head list
-
-		if( node1.data < node2.data ){
+		if( node1.val < node2.val ){
 			head = node1;
-			node = head; 
+			node = head;
 			node1 = node1.next;
 		}
 		else{
@@ -49,7 +21,7 @@
 		}
 
 		while( node1 && node2 ){
-			if( node1.data < node2.data ){
+			if( node1.val<node2.val ){
 				node.next = node1;
 				node = node.next;
 				node1 = node1.next;
@@ -74,70 +46,42 @@
 		}
 
 		node.next = null;
-
 		return head;
 	};
 
-	var getLength = function(head){
-		var n = 0;
-		while(head){
-			n++;
-			head = head.next;
-		}
-		return n;
-	};
-
-	var getNthNode = function(head, n){
-		if( !head ) return null;
-
-		var m = 0;
-		while( head && m<n ){
-			head = head.next;
-			m++;
-		}
-
-		if( head && m==n ){
-			return head;
-		}
-		return null;
-	};
-
 	var mergeSort = function(head){
-		if( !head || !head.next ) return head;
 
-		var n = getLength(head),
-				m = Math.floor((n+1)/2)-1,
-				node = getNthNode(head, m),
-				head1, head2;
+		if( !head || !head.next )	return head;
 
-		if( !node )	return head;
+		var n=0,
+				node = head,
+				m,
+				i;
 
-		head1 = head;
-		head2 = node.next;
-		node.next = null; // disconnected two lists
+		while(node){
+			n++;
+			node = node.next;
+		}
 
-		head1 = mergeSort(head1);
-		head2 = mergeSort(head2);
-		return mergeSortedLinkedList(head1, head2);
+		m = Math.floor((n-1)/2);
+
+		i = 0;
+		node = head;
+		while( i<m ){
+			node = node.next;
+			i++;
+		}
+
+		var head1 = head;
+		var head2 = node.next;
+
+		node.next = null;	// cut the connection between two lists
+
+		var h1 = mergeSort(head1),
+				h2 = mergeSort(head2);
+
+		return merge( h1, h2 );
 	};
-
-	(function(){
-		var l = new LinkedList();
-		l.append(1);
-		l.append(5);
-		l.append(3);
-		l.append(99);
-		l.append(63);
-		l.append(78);
-		l.append(24);
-		l.append(-99);
-		l.append(0);
-
-		
-		var h = mergeSort(l.head);
-		print(h);
-
-	})();
 
 
 
